12e6e4eea4cfc261fee25f3b9e7c0884d3d16c63
Kierra M <k.merriman32@hotmail.com>
2019-08-31 17:59:36 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -3,13 +3,17 @@ class Person
   def initialize (persons_name)
     @name = persons_name
   end
-
-  def name=(persons_name)
-    @name = persons_name
-  end
-
-  def name
-    @name
-  end
-  
 end
+
+
+# refractor above
+#class Person 
+#def name=(persons_name)
+ #   @name = persons_name
+ # end
+
+ # def name
+ #   @name
+ # end
+  
+#end
